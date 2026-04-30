202755115471d36aa671f679b13ceda92c3f8cd8
david11fd <Davidgtjuega@gmail.com>
2026-04-30 10:08:32 +0200

    Test unitarios completos y Comprobados, todos pasando

--- demo/src/test/java/com/example/AppTest.java
+++ demo/src/test/java/com/example/AppTest.java
@@ -1,20 +1,63 @@
 package com.example;
 
-import static org.junit.Assert.assertTrue;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.Test;
+public class AppTest {
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    void libroRecienCreadoTieneTituloCorrector() {
+        Libro libro = new Libro("El Quijote");
+        assertEquals("El Quijote", libro.getTitulo());
+    }
+
+    @Test
+    void libroRecienCreadoNoEstaPrestado() {
+        Libro libro = new Libro("El Quijote");
+        assertFalse(libro.isPrestado());
+    }
+
+    @Test
+    void prestarLibroCambiaSuEstado() {
+        Libro libro = new Libro("El Quijote");
+        libro.prestar();
+        assertTrue(libro.isPrestado());
+    }
+
+    @Test
+    void prestarLibroYaPrestadoLanzaExcepcion() {
+        Libro libro = new Libro("El Quijote");
+        libro.prestar();
+        assertThrows(IllegalStateException.class, () -> libro.prestar());
+    }
+
+    @Test
+    void devolverLibroCambiaSuEstado() {
+        Libro libro = new Libro("El Quijote");
+        libro.prestar();
+        libro.devolver();
+        assertFalse(libro.isPrestado());
+    }
+
+    @Test
+    void devolverLibroNoPrestadoLanzaExcepcion() {
+        Libro libro = new Libro("El Quijote");
+        assertThrows(IllegalStateException.class, () -> libro.devolver());
+    }
+
+    @Test
+    void añadirUnLibroABiblioteca() {
+        Biblioteca biblioteca = new Biblioteca();
+        biblioteca.añadirLibro(new Libro("El Quijote"));
+        assertEquals(1, biblioteca.getNumeroLibros());
+    }
+
+    @Test
+    void añadirVariosLibrosABiblioteca() {
+        Biblioteca biblioteca = new Biblioteca();
+        biblioteca.añadirLibro(new Libro("El Quijote"));
+        biblioteca.añadirLibro(new Libro("1984"));
+        biblioteca.añadirLibro(new Libro("Dune"));
+        assertEquals(3, biblioteca.getNumeroLibros());
     }
 }
